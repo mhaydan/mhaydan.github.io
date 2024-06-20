eaa830173b399880cb00ba74754561e305589270
mhaydan <135733500+mhaydan@users.noreply.github.com>
2024-06-20 18:00:49 +0200

    Add files via upload

---
+++ files/webmap/data/BuiltEnvironmentTimeline_15.js
@@ -0,0 +1,1 @@
+var json_BuiltEnvironmentTimeline_15 = {"type":"FeatureCollection","name":"BuiltEnvironmentTimeline_15","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
